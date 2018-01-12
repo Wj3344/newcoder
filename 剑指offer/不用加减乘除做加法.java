4749520878c59c6f734c9c290b666f261d14b289
fanchen <1441088980@qq.com>
2018-01-13 00:08:36 +0800

    不用加减乘除做加法

---
+++ 剑指offer/不用加减乘除做加法.java
@@ -0,0 +1,14 @@
+/**
+ * 答案参考
+ * https://www.jianshu.com/p/55166bfd31dd
+ */
+public class Solution {
+    public int Add(int num1, int num2) {
+        while (num2!=0) {
+            int a = num1^num2;
+            num2 = (num1&num2)<<1;
+            num1 = a;
+        }
+        return num1;
+    }
+}
